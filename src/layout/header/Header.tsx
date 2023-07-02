446496cb2d96f5d8c076b4fb4a6f0e474738128f
박종유 <jypark@nextree.io>
2023-07-02 23:12:25 +0900

    feat: useMediaQuery 에 따른 컴포넌트 나누기

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,27 +1,23 @@
 import { useMediaQuery } from 'react-responsive';
 import { Logo } from './components/Logo';
+import { DeskTop } from './components/DeskTop';
+import { Mobile } from './components/Mobile';
 
 export const Header = () => {
   const isMobile = useMediaQuery({
     query: '(min-width:0px) and (max-width:639px)',
   });
 
-  const isTablet = useMediaQuery({
-    query: '(min-width:640px) and (max-width:1023px)',
-  });
-
   const isDesktop = useMediaQuery({
-    query: '(min-width:1024px)',
+    query: '(min-width:640px)',
   });
 
   return (
     <nav className="header nav">
       <div className="inner_container">
         <Logo />
-
-        {isMobile && <>모바일 Header</>}
-        {isTablet && <>테블릿 Header</>}
-        {isDesktop && <>데스크탑 Header</>}
+        {isMobile && <Mobile />}
+        {isDesktop && <DeskTop />}
       </div>
     </nav>
   );
---
+++ src/layout/header/components/DeskTop.tsx
@@ -0,0 +1,3 @@
+export const DeskTop = () => {
+  return <div>DeskTop</div>;
+};
