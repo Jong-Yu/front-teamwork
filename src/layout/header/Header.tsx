77f82d7acc013a985af03ee40e6229a11f720d15
박종유 <jypark@nextree.io>
2023-07-02 22:19:40 +0900

    feat: useMediaQuery 사용해보기

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,3 +1,28 @@
+import { useMediaQuery } from 'react-responsive';
+import { Logo } from './components/Logo';
+
 export const Header = () => {
-  return <div className="header layout">큰 머리다!</div>;
+  const isMobile = useMediaQuery({
+    query: '(min-width:0px) and (max-width:639px)',
+  });
+
+  const isTablet = useMediaQuery({
+    query: '(min-width:640px) and (max-width:1023px)',
+  });
+
+  const isDesktop = useMediaQuery({
+    query: '(min-width:1024px)',
+  });
+
+  return (
+    <nav className="header nav">
+      <div className="inner_container">
+        <Logo />
+
+        {isMobile && <>모바일 Header</>}
+        {isTablet && <>테블릿 Header</>}
+        {isDesktop && <>데스크탑 Header</>}
+      </div>
+    </nav>
+  );
 };
