55650cf8d22db5dc26f0f2e955ac96ddffeb8d9a
박종유 <jypark@nextree.io>
2023-07-04 18:00:57 +0900

    feat: Toss 메인화면 클론( animation )

--- src/main/components/TitleSection.tsx
+++ src/main/components/TitleSection.tsx
@@ -1,8 +1,15 @@
 import { keyframes } from '@emotion/react';
 import { MOBILE_MEDIA } from '../../shared/hooks/Responsive/Responsive.hooks';
+import { Scrolldown } from './scrolldown';
+import { useRef } from 'react';
 
 export const TitleSection = () => {
   const animation = keyframes({ from: { opacity: 0 }, to: { opacity: 1 } });
+  const ref = useRef<HTMLDivElement>(null);
+
+  const onClick = () => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <section>
@@ -80,16 +87,174 @@ export const TitleSection = () => {
             >
               {`동호회의 모든 것 \n팀 워크에서 쉽고 빠르게`}
             </h1>
-            <div css={{ width: '300px' }}>
-              <button className="p-button">다운로드</button>
-              <button className="p-button">회원가입</button>
-            </div>
           </div>
 
-          <div>
-            <button>aa</button>
-          </div>
+          <button
+            css={{
+              background: 'none',
+              border: '0px',
+              padding: '0px',
+              outline: 'none',
+              cursor: 'pointer',
+              marginBottom: '30px',
+            }}
+            onClick={onClick}
+          >
+            <div
+              css={{
+                width: '50px',
+                height: '50px',
+              }}
+            >
+              <Scrolldown />
+            </div>
+          </button>
         </div>
+      </div>
+      <div
+        ref={ref}
+        css={{
+          position: 'relative',
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          padding: '200px 0',
+          background: '#f9fafb',
+        }}
+      >
+        <p
+          css={{
+            fontSize: '32px',
+            fontWeight: '700',
+            lineHeight: '1.6',
+            color: '#191f28',
+            textAlign: 'center',
+            marginBottom: '50px',
+          }}
+        >
+          {`내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.\n이제껏 경험 못 했던 쉽고 편리한 금융 서비스,\n토스와 함께라면 당신의 일상이 새로워질 거예요.`}
+        </p>
+      </div>
+      <div
+        css={{
+          position: 'relative',
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          padding: '200px 0',
+          background: '#f9fafb',
+        }}
+      >
+        <p
+          css={{
+            fontSize: '32px',
+            fontWeight: '700',
+            lineHeight: '1.6',
+            color: '#191f28',
+            textAlign: 'center',
+            marginBottom: '50px',
+          }}
+        >
+          {`내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.\n이제껏 경험 못 했던 쉽고 편리한 금융 서비스,\n토스와 함께라면 당신의 일상이 새로워질 거예요.`}
+        </p>
+      </div>
+      <div
+        css={{
+          position: 'relative',
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          padding: '200px 0',
+          background: '#f9fafb',
+        }}
+      >
+        <p
+          css={{
+            fontSize: '32px',
+            fontWeight: '700',
+            lineHeight: '1.6',
+            color: '#191f28',
+            textAlign: 'center',
+            marginBottom: '50px',
+          }}
+        >
+          {`내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.\n이제껏 경험 못 했던 쉽고 편리한 금융 서비스,\n토스와 함께라면 당신의 일상이 새로워질 거예요.`}
+        </p>
+      </div>
+      <div
+        css={{
+          position: 'relative',
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          padding: '200px 0',
+          background: '#f9fafb',
+        }}
+      >
+        <p
+          css={{
+            fontSize: '32px',
+            fontWeight: '700',
+            lineHeight: '1.6',
+            color: '#191f28',
+            textAlign: 'center',
+            marginBottom: '50px',
+          }}
+        >
+          {`내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.\n이제껏 경험 못 했던 쉽고 편리한 금융 서비스,\n토스와 함께라면 당신의 일상이 새로워질 거예요.`}
+        </p>
+      </div>
+      <div
+        css={{
+          position: 'relative',
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          padding: '200px 0',
+          background: '#f9fafb',
+        }}
+      >
+        <p
+          css={{
+            fontSize: '32px',
+            fontWeight: '700',
+            lineHeight: '1.6',
+            color: '#191f28',
+            textAlign: 'center',
+            marginBottom: '50px',
+          }}
+        >
+          {`내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.\n이제껏 경험 못 했던 쉽고 편리한 금융 서비스,\n토스와 함께라면 당신의 일상이 새로워질 거예요.`}
+        </p>
+      </div>
+      <div
+        css={{
+          position: 'relative',
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          padding: '200px 0',
+          background: '#f9fafb',
+        }}
+      >
+        <p
+          css={{
+            fontSize: '32px',
+            fontWeight: '700',
+            lineHeight: '1.6',
+            color: '#191f28',
+            textAlign: 'center',
+            marginBottom: '50px',
+          }}
+        >
+          {`내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.\n이제껏 경험 못 했던 쉽고 편리한 금융 서비스,\n토스와 함께라면 당신의 일상이 새로워질 거예요.`}
+        </p>
       </div>
     </section>
   );
