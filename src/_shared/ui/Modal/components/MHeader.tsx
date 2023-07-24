eb9b75111815aca4d7eb28d993703e36c0325941
박종유 <jypark@nextree.io>
2023-07-24 18:02:48 +0900

    feat: Modal Header 에 x 버튼 옮김

--- src/_shared/ui/Modal/components/MHeader.tsx
+++ src/_shared/ui/Modal/components/MHeader.tsx
@@ -4,10 +4,45 @@ const Header = styled.div({
   padding: '1rem 1.5rem',
 });
 
+const Container = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+const ExitButton = styled.span({
+  position: 'relative',
+  width: '32px',
+  height: '32px',
+  cursor: 'pointer',
+  '&:after, &:before': {
+    position: 'absolute',
+    left: '15px',
+    top: '5px',
+    content: "''",
+    height: '24px',
+    width: '1px',
+    backgroundColor: '#282B33',
+    transform: 'rotate(-45deg)',
+  },
+  '&:before': {
+    transform: 'rotate(45deg)',
+  },
+  '&:hover': {
+    transform: 'opacity(0.3)',
+  },
+});
+
 interface MHeaderProps {
   children?: React.ReactNode;
+  onClose: () => void;
 }
 
-export const MHeader = ({ children }: MHeaderProps) => {
-  return <Header>{children}</Header>;
+export const MHeader = ({ children, onClose }: MHeaderProps) => {
+  return (
+    <Header>
+      <Container>{children}</Container>
+      <ExitButton onClick={onClose} />
+    </Header>
+  );
 };
