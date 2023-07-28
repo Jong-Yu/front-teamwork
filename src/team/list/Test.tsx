719e66976870789fbaca400eb625a44931f423fd
박종유 <jypark@nextree.io>
2023-07-28 13:39:23 +0900

    feat: 임시 로그아웃 추가

--- src/team/list/Test.tsx
+++ src/team/list/Test.tsx
@@ -1,13 +1,26 @@
+import { logout } from '../../_query/auth/api/AuthApi';
+import { useGoPush } from '../../_shared/hooks/History/History.hooks';
 import { getKakao } from '../../_shared/util/KaKao/kakao.util';
 
 export const Test = () => {
+  //
+  const goPush = useGoPush();
+
   const onClick = () => {
     getKakao();
   };
 
+  const onClickLogout = () => {
+    logout().then(() => goPush('/auth/login'));
+  };
+
   return (
-    <button id="kakaotalk-sharing-btn" onClick={onClick}>
-      Test
-    </button>
+    <>
+      <button id="kakaotalk-sharing-btn" onClick={onClick}>
+        공유하기 테스트
+      </button>
+
+      <button onClick={onClickLogout}>임시 로그아웃</button>
+    </>
   );
 };
