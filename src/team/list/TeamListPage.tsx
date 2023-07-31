85407d76225c2654b2e3d1947fd8270f35cfce38
박종유 <jypark@nextree.io>
2023-07-31 14:50:45 +0900

    temp: TeamList 임시 주석

--- src/team/list/TeamListPage.tsx
+++ src/team/list/TeamListPage.tsx
@@ -1,6 +1,6 @@
-import { TeamCreateModal } from '../create/TeamCreateModal';
+// import { TeamCreateModal } from '../create/TeamCreateModal';
 import { Header } from './components/Header';
-import { TeamList } from './components/TeamList';
+// import { TeamList } from './components/TeamList';
 import { Map } from '../../_shared/ui/Map/Map';
 import { Test } from './Test';
 
