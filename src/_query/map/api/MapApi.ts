ae7fddc24ee00a9f61b7716abf71c9d5b0eb14ce
박종유 <jypark@nextree.io>
2023-07-31 11:30:42 +0900

    feat: kakao 검색 api axios 변경

--- src/_query/map/api/MapApi.ts
+++ src/_query/map/api/MapApi.ts
@@ -1,16 +1,16 @@
-import { getAxios } from '../../../_shared/util/Axios/axios.util';
+import axios from 'axios';
 import { SearchPlaces } from '../model/SearchPlaces';
 
 export function searchKeyWord(keyword: string) {
-  const { get, axiosReturn } = getAxios();
-
   // 검색 결과를 가져옵니다.
-  return get<SearchPlaces>(
-    `https://dapi.kakao.com/v2/local/search/keyword.json?query=${keyword}`,
-    {
-      headers: {
-        Authorization: `KakaoAK ${import.meta.env.VITE_KAKAO_REST_API_KEY}`,
+  return axios
+    .get<SearchPlaces>(
+      `https://dapi.kakao.com/v2/local/search/keyword.json?query=${keyword}`,
+      {
+        headers: {
+          Authorization: `KakaoAK ${import.meta.env.VITE_KAKAO_REST_API_KEY}`,
+        },
       },
-    },
-  ).then(axiosReturn);
+    )
+    .then(res => res.data);
 }
